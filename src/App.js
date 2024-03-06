e9e813ebeaf4fd3742862cd6d42b32b407f735ab
kyh012412 <kyh0124.12@gmail.com>
2024-03-06 12:40:07 +0900

    ref와 ref가 아닌 var의 차이

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count,setCount] = useState(0);
+  const [renderer,setRenderer] = useState(0);
   const countRef = useRef(0);
   
+  //countVar는 랜더링이 다시 될때 초기화가됨
+  let countVar = 0;
+
   console.log('🎉렌더링...');
 
-  const increaseCountState = () =>{
-    setCount(count +1);
-  }
-
-  const increaseRefState = () =>{
+  const increaseRef = () =>{
     countRef.current = countRef.current +1;
     console.log(countRef.current);
   }
 
+  const increaseVar = () =>{
+    countVar = countVar +1;
+    console.log('var: ',countVar);
+  }
+  const doRender = ()=> {
+    setRenderer(renderer+1);
+  }
+
+  const printResults = () =>{
+    console.log(`ref: ${countRef.current}, var: ${countVar}`);
+  }
+
   return (
     <div>
-      <p>Stage: {count}</p>
       <p>Ref: {countRef.current}</p>
-      <button onClick={increaseCountState}>State 올리기</button>      
-      <button onClick={increaseRefState}>Ref 올리기</button>
+      <p>Var: {countVar}</p>
+      <button onClick={increaseRef}>Ref 올리기</button>      
+      <button onClick={increaseVar}>Var 올리기</button>
+      <button onClick={doRender}>렌더!</button>
+      <button onClick={printResults}>Ref Var 값 출력</button>
     </div>
   );
 }
